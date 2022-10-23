ba66f6311f1158d6e2863af2634f761c719570ad
ilija <komperdel@gmail.com>
2022-10-23 16:18:11 +0300

    finish task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Drawers from './images/drawers.jpg';
+import ShareIcon from './images/icon-share.svg';
+import PinterestIcon from './images/icon-pinterest.svg';
+import TwitterIcon from './images/icon-twitter.svg';
+import FacebookIcon from './images/icon-facebook.svg';
+import userImg from './images/avatar-michelle.jpg';
+
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [isShown, setIsShown] = useState(false);
+
+	return (
+		<div className="App">
+			<div className="container">
+				<div className="imageContainer">
+					<img src={Drawers} alt="drawers" />
+				</div>
+				<div className="text">
+					<h3>
+						Shift the overall look and feel by adding these wonderful touches to furniture in your
+						home
+					</h3>
+					<p>
+						Ever been in a room and felt like something was missing? Perhaps it felt slightly bare
+						and uninviting. I’ve got some simple tips to help you make any room feel complete.
+					</p>
+					<div className="userInfo">
+						<img className="userImg" src={userImg} alt="" />
+						<div className="userPost">
+							<h3>Michelle Appleton</h3>
+							<p> 28 Jun 2020</p>
+						</div>
+						<img className="share" src={ShareIcon} alt="" onClick={() => setIsShown(!isShown)} />
+						{isShown && (
+							<div className="popupshows">
+								<div className="socialIcon">
+									S H A R E
+									<img src={FacebookIcon} alt="FacebookIcon" />
+									<img src={TwitterIcon} alt="TwitterIcon" />
+									<img src={PinterestIcon} alt="PinterestIcon" />
+								</div>
+							</div>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
